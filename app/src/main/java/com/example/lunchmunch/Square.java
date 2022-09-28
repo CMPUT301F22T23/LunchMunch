f5306ae98f9f20ce0ae48e2b5a5ad82db78a59aa
Tosin Kuye <tkuye77@gmail.com>
2022-09-27 19:23:45 -0600

    Merge Fix

--- app/src/main/java/com/example/lunchmunch/Square.java
+++ app/src/main/java/com/example/lunchmunch/Square.java
@@ -1,9 +1,8 @@
 package com.example.lunchmunch;
 
 public class Square extends Shape {
-    private String color = "red";
+
     public Square() {
-        
 
     }
 }
--- app/src/main/java/com/example/lunchmunch/Star.java
+++ app/src/main/java/com/example/lunchmunch/Star.java
@@ -2,7 +2,6 @@ package com.example.lunchmunch;
 
 public class Star extends Shape{
     Integer temperature;
-
     public Star () {
         temperature = 10000000;
     }
