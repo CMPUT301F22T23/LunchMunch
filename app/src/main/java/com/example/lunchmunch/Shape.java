70f513d774d28782aaf6489d7ce1d504bde02618
MW <mw@Maxyms-MacBook-Pro-8.local>
2022-10-23 11:01:37 -0600

    aa

--- app/src/main/java/com/example/lunchmunch/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.example.lunchmunch;
-
-public abstract class Shape {
-    private Integer x;
-    private Integer y;
-    String color = "purple";
-}
--- app/src/main/java/com/example/lunchmunch/Trapezoid.java
+++
@@ -1,9 +0,0 @@
-package com.example.lunchmunch;
-
-public class Trapezoid extends Shape{
-    private Integer temperature;
-
-    public Trapezoid() {
-        this.temperature = 1;
-    }
-}
