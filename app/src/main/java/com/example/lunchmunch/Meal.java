9bc4f8b1b604dc8e6703dd67911dfd7b8e4cf31a
Vansh Srivastav <vanshsrivastav@yahoo.ca>
2022-10-23 16:16:16 -0600

    Added Meal and MealPlan classes for later functionality

---
+++ app/src/main/java/com/example/lunchmunch/Meal.java
@@ -0,0 +1,31 @@
+package com.example.lunchmunch;
+
+import java.util.ArrayList;
+
+public class Meal {
+/*
+
+    private Recipe recipe;
+    private ArrayList<Ingredient> addiIngr;
+
+    public Meal(Recipe r, ArrayList<Ingredient> additI){
+        this.r = recipe;
+        this.additI = addiIngr;
+    }
+
+
+    public void getIngredients(Recipe r){
+        // code to get Ingredients in Recipe
+    }
+
+    public void deleteRecipe(Recipe r){
+        // code to delete Recipe
+    }
+    public void editRecipe(Recipe r){
+        // code to edit Recipe
+    }
+
+*/
+
+}
+
