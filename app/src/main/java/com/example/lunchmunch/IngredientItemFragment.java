d42531d28684f84d538db2f135480ef22efc85c9
Tobi Ogunleye <tobiogunleye99@gmail.com>
2022-10-25 12:56:17 -0600

    update xml file to resemble mock up

--- app/src/main/java/com/example/lunchmunch/IngredientItemFragment.java
+++ app/src/main/java/com/example/lunchmunch/IngredientItemFragment.java
@@ -1,2 +1,11 @@
-package com.example.lunchmunch;public class IngredientItemFragment {
+package com.example.lunchmunch;
+
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+public class IngredientItemFragment extends Fragment {
+    private EditText ingredient_desc;
+    private EditText ingredient_price;
+
 }
